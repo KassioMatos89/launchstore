59f3126d84555aee381395fcdef71b5cb650c5c0
unknown <kassio_29@hotmail.com>
2020-03-05 23:19:39 -0300

    Feature: Cadastro de produtos com formatacao de valores no campo price

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ routes.get("/", function(req, res){
 
 routes.get('/products/create', ProductController.create)
 routes.post('/products', ProductController.post)
+routes.get('/products/:id/edit', ProductController.edit)
 
 // Alias
 routes.get('/ads/create', function ( req, res ) {
